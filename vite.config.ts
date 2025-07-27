a33d460d092a4795350f5862e5ce697967f9cbce
aviy453 <132845530+aviy453@users.noreply.github.com>
2025-07-27 10:45:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyCs2h3hMo2PvlFD7AWGJ3x_47hi0VhQJS8)
       },
       resolve: {
         alias: {
